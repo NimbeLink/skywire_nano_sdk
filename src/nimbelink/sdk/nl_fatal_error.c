aac7677eb1d75d0405a574b2ce872772bea555e7
Erik Johnson <joh06937@gmail.com>
2021-03-04 21:21:35 -0600

    NL Fatal Error: Removed requesting reboot
    The reboot doesn't actually let the Secure firmware print the crash dump, so the
    previous approach didn't really help at all with debugging.
    
    Additionally, the Secure firmware will catch this, display the fault, and
    continue operating anyway, meaning this isn't required to prevent a boot loop or
    something similar.
    
    If the old behavior is desired, Nordic's SDK already provides that functionality
    with their RESET_ON_FATAL_ERROR configuration.

--- src/nimbelink/sdk/nl_fatal_error.c
+++ src/nimbelink/sdk/nl_fatal_error.c
@@ -12,9 +12,11 @@
  * portions are excluded from the preceding copyright notice of NimbeLink Corp.
  */
 #include <arch/cpu.h>
+#include <autoconf.h>
 #include <fatal.h>
 #include <logging/log.h>
 #include <logging/log_ctrl.h>
+#include <sys/util.h>
 
 #include "nimbelink/sdk/secure_services/kernel.h"
 
@@ -24,8 +26,7 @@ LOG_MODULE_REGISTER(fatal_error, LOG_LEVEL_INF);
  * \brief Handles a fatal system error
  *
  *  This will allow the fatal error to go unhandled, allowing the Secure
- *  firmware to handle the error and prevent the Non-Secure application from
- *  being launched during the next, immediate reboot.
+ *  firmware to handle the error and stop the Non-Secure firmware's execution.
  *
  * \param reason
  *      The reason for the fatal error
@@ -36,12 +37,8 @@ LOG_MODULE_REGISTER(fatal_error, LOG_LEVEL_INF);
  */
 void k_sys_fatal_error_handler(unsigned int reason, const z_arch_esf_t *esf)
 {
-	(void)esf;
-	(void)reason;
+    (void)esf;
+    (void)reason;
 
-	LOG_ERR("Resetting the system");
-	LOG_WRN("Preventing application from running next boot");
-	LOG_PANIC();
-
-    Kernel_Reset(KERNEL_RESET_SKIP_LAUNCH);
+    LOG_PANIC();
 }
