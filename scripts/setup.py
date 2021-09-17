1ebcfc3a944ade5d4689927b937f6ca47edd5281
Erik Johnson <erik.johnson@nimbelink.com>
2021-09-17 10:57:35 -0500

    Scripts: Added our new separate pynl-cell Python package

--- scripts/setup.py
+++ scripts/setup.py
@@ -61,6 +61,6 @@ setup(
     install_requires = [
         "imgtool==1.7.2",
 
-        "pynl-base @ git+https://github.com/NimbeLink/pynl-base@v1.0.2"
+        "pynl-cell @ git+https://github.com/NimbeLink/pynl-cell@v1.0.0"
     ]
 )
